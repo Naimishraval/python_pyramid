267289e8c5794d766cb64835346258f232b20e40
Naimish Raval <sonagaranayanbharatbhai@gmail.com>
2025-11-30 19:24:17 +0530

    Update p 44.py

--- p 44.py
+++ p 44.py
@@ -1,6 +1,14 @@
-def pattern44():
-    s = "1#3#5"
-    for i in range(len(s)):
-        print(s[:len(s)-i])
-
-pattern44()
+no=int(input("enter limit")
+n=1
+m=1
+for i in range(no,0,-1):
+    for j in range(i,0,-1):
+        if n%2==0:
+            print("#",end=" ")
+        else:
+            print("",m,end=" ")
+            m=m+2
+            n=n+1
+    n=1
+    m=1
+    print()
